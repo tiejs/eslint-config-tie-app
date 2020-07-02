87db4e850659ba64709b780163f568bcfcf3ad89
forsigner <forsigner@gmail.com>
2020-07-03 02:00:03 +0800

    feat: extend a11y and import

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "prettier/@typescript-eslint",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
   ],
