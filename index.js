1e4ac4ab98ad1c9db0f448d2c6a7dae0d2a9eecb
forsigner <forsigner@gmail.com>
2020-07-03 14:50:33 +0800

    chore: revert

--- index.js
+++ index.js
@@ -3,33 +3,17 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "react-hooks"],
-  extends: [
-    "prettier/@typescript-eslint",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/recommended",
-    "react-native",
-    "plugin:react-hooks/recommended",
-  ],
+  plugins: ["@typescript-eslint"],
+  extends: ["prettier/@typescript-eslint", "plugin:prettier/recommended"],
   env: {
     commonjs: true,
     es6: true,
     jest: true,
     node: true,
   },
+
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2019,
     sourceType: "module",
-  },
-  rules: {
-    "import/namespace": "off",
-    "react/prop-types": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "jsx-quotes": ["error", "prefer-double"],
   },
 };
